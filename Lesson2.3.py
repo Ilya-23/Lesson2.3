646ecaca64fe966edc99552435fb9f50a4428778
Ilya-23 <ilya.gawrin.7635@gmail.com>
2024-09-09 13:03:10 +0700

    Add files via upload

--- Lesson2.3.py
+++ Lesson2.3.py
@@ -2,7 +2,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 print('Ряд чисел: ',my_list)
 print('Положительные числа до первого отрицательного:')
 index = 0
-while index >= 0 or index <= len(my_list):
+while index >= 0 or index <= len(my_list)-1:
     number = my_list[index]
     if number == 0:
         index += 1
